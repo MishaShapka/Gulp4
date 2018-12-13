8a3a10d6d3bfe78d2c35b04df5023c18a3133123
mishashapka <mishashapka@icloud.com>
2018-12-13 18:32:40 +0300

    @

--- gulp/task/sass.js
+++ gulp/task/sass.js
@@ -1,7 +1,7 @@
 module.exports = function() {
-    // $.gulp.task('sass', function () {
-    //     return gulp.src('./src/sass/**/*.scss')
-    //       .pipe(sass().on('error', sass.logError))
-    //       .pipe(gulp.dest('./css'));
-    //   });
+    $.gulp.task('sass', function () {
+        return gulp.src('./src/sass/**/*.scss')
+          .pipe(sass().on('error', sass.logError))
+          .pipe(gulp.dest('./css'));
+      });
 };
